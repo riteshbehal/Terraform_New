ed8744d48fd80ccd98c9100db6d6fa71aa9c6237
Ritesh Behal <49110318+riteshbehal@users.noreply.github.com>
2026-06-26 16:44:00 +0530

    Refactor variables in variables.tf for type safety
    Updated variable types and default values for Security_Group and AMIS.

--- 3_terraform_concept_buliding_block/1_nginx_demo/Resources/variables.tf
+++ 3_terraform_concept_buliding_block/1_nginx_demo/Resources/variables.tf
@@ -10,12 +10,12 @@ default = "us-east-1"
 }
 
 variable "Security_Group"{
-    type = list
-    default = ["sg-24076", "sg-90890", "sg-456789"]
+    type = list(string)
+    default = ["sg-03480845964d1ed46", "sg-092cd5a7a3aa7d3e8", "sg-0aab1919622996c1c"]
 }
 
 variable "AMIS" {
-    type = map
+    type = map(string)
     default = {
         us-east-1 = "ami-0b6d9d3d33ba97d99"
         us-east-2 = "ami-05692172625678b4e"
