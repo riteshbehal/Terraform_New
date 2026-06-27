ac36853f4bec852ceb2b429279e37c7c0ca4f65c
Ritesh Behal <49110318+riteshbehal@users.noreply.github.com>
2026-06-27 21:05:05 +0530

    Modify Terraform variable defaults for AWS settings
    Updated default values for AWS_REGION, Security_Group, and AMIS variables.

--- 2_terraform_variable_detailed_explaination/2/Resousces/variables.tf
+++ 2_terraform_variable_detailed_explaination/2/Resousces/variables.tf
@@ -3,18 +3,18 @@ variable "AWS_ACCESS_KEY" {}
 variable "AWS_SECRET_KEY" {}
 
 variable "AWS_REGION" {
-default = "us-east-1"
+default = "ap-south-1"
 }
 
 variable "Security_Group"{
     type = list(string)
-    default = ["sg-03480845964d1ed46", "sg-092cd5a7a3aa7d3e8", "sg-0aab1919622996c1c"]
+    default = ["sg-0cd2d0237a8aba1b0", "sg-030c4b47d10d7f71c", "sg-0be7cec84a04dd053"]
 }
 
 variable "AMIS" {
     type = map(string)
     default = {
-        us-east-1 = "ami-0f40c8f97004632f9"
+        ap-south-1 = "ami-0bc7aabcf58d1e02a"
         us-east-2 = "ami-05692172625678b4e"
         us-west-2 = "ami-0352d5a37fb4f603f"
         us-west-1 = "ami-0f40c8f97004632f9"
