458cf639eadfa7df9be905367b84361e573c45a6
Ritesh <behal.ritesh@gmail.com>
2026-06-27 21:18:16 +0530

    adding s3 bucket code

---
+++ 2_terraform_variable_detailed_explaination/3/Resousces/creates3.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = var.bucket_name
+
+  tags = {
+    Name        = var.bucket_name
+    Environment = var.environment
+    CreatedBy   = "Terraform"
+  }
+}
