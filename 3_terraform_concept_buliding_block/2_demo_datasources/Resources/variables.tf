94578ad2f363bc379c4d906341047002265a534f
Ritesh Behal <49110318+riteshbehal@users.noreply.github.com>
2026-06-27 16:31:58 +0530

    Update AWS_REGION and Security_Group variables
    Updated AWS_REGION default value and modified Security_Group variable type and default values.

--- 3_terraform_concept_buliding_block/2_demo_datasources/Resources/variables.tf
+++ 3_terraform_concept_buliding_block/2_demo_datasources/Resources/variables.tf
@@ -1,24 +1,14 @@
 variable "AWS_ACCESS_KEY" {
     type = string
-    default = "AKIASMSIZOF42P2VUDSZ"
 }
 
 variable "AWS_SECRET_KEY" {}
 
 variable "AWS_REGION" {
-default = "us-east-1"
+default = "ap-south-1"
 }
 
 variable "Security_Group"{
-    type = list
-    default = ["sg-24076", "sg-90890", "sg-456789"]
+    type = list(string)
+    default = ["sg-03480845964d1ed46", "sg-092cd5a7a3aa7d3e8", "sg-0aab1919622996c1c"]
 }
-
-variable "AMIS" {
-    type = map
-    default = {
-        us-east-1 = "ami-0b6d9d3d33ba97d99"
-        us-east-2 = "ami-05692172625678b4e"
-    }
-}
-
