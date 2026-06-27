39a04832967355c55be5670c6163a8209df4e564
Ritesh Behal <49110318+riteshbehal@users.noreply.github.com>
2026-06-27 16:34:58 +0530

    Fix tags syntax in createInstance.tf

--- 3_terraform_concept_buliding_block/2_demo_datasources/Resources/createInstance.tf
+++ 3_terraform_concept_buliding_block/2_demo_datasources/Resources/createInstance.tf
@@ -21,7 +21,7 @@ resource "aws_instance" "MyFirstInstnace" {
   instance_type = "t2.micro"
   availability_zone = data.aws_availability_zones.avilable.names[1]
 
-  tag = {
+  tags = {
    Name = "custom_instance"
   }
 }
